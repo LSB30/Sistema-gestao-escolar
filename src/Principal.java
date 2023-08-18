7e4e620922434ff43f60dd0454aa57ce83fb54f7
Lucas Santana <122113277+llink7@users.noreply.github.com>
2023-08-18 11:24:23 -0300

    init

---
+++ src/Principal.java
@@ -0,0 +1,10 @@
+public class Principal {
+    public static void main(String[] args) {
+        System.out.println("Bem-vindo ao sistema de gestão escolar");
+
+        String nome = "Lucas Santana Belato";
+        int idade = 22;
+        double nota = 8.6;
+        
+    }
+}
